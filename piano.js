1e2455adfa2ec2516330b608885269b7bc1a1982
Jasper Eenhoorn <een@emmauscollege.nl>
2026-07-01 01:15:57 +0200

    play white notes too

--- piano.js
+++ piano.js
@@ -1,12 +1,20 @@
 const synth = new Tone.Synth().toDestination();
 
-const span_notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"];
+const spans_notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"];
 const btns_notes = ["C#4", "D#4", "F#4", "G#4", "A#4"];
+
+const spans = document.querySelectorAll("span.white-key");
+for(let i=0; i<spans.length; i++) {
+	spans[i].addEventListener('click', async () => {
+		await Tone.start();
+		synth.triggerAttackRelease(spans_notes[i % spans_notes.length], '4n');
+	});
+}
 
 const btns = document.querySelectorAll("button");
 for(let i=0; i<btns.length; i++) {
 	btns[i].addEventListener('click', async () => {
 		await Tone.start();
-		synth.triggerAttackRelease(btns_notes[i], '4n') 
+		synth.triggerAttackRelease(btns_notes[i % btns_notes.length], '4n');
 	});
 }
