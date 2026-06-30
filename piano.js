7fe1b79fb27204917641c21ca3d4bc7f73a03c3d
Jasper Eenhoorn <een@emmauscollege.nl>
2026-07-01 01:45:30 +0200

    trigger mouseover playing with click

--- piano.js
+++ piano.js
@@ -1,5 +1,7 @@
 const synth = new Tone.Synth().toDestination();
 
+let playing = false;
+
 let spans_octave = 4;
 let btns_octave = 4;
 
@@ -7,25 +9,40 @@ const spans_notes = ["C", "D", "E", "F", "G", "A", "B"];
 const btns_notes = ["C#", "D#", "F#", "G#", "A#"];
 
 const spans = document.querySelectorAll("span.white-key");
-for(let i=0; i<spans.length; i++) {
-	const note_name = spans_notes[i % spans_notes.length];
-	const note = `${note_name}${spans_octave}`;
-	spans[i].addEventListener('click', async () => {
-		await Tone.start();
-		synth.triggerAttackRelease(note, '4n');
-		// console.log(note);
-	});
-	if(i%spans_notes.length === spans_notes.length-1) { spans_octave++; }
-}
-
 const btns = document.querySelectorAll("button");
-for(let i=0; i<btns.length; i++) {
-	const note_name = btns_notes[i % btns_notes.length];
-	const note = `${note_name}${btns_octave}`;
-	btns[i].addEventListener('click', async () => {
-		await Tone.start();
-		synth.triggerAttackRelease(note, '4n');
-		// console.log(note);
-	});
-	if(i%btns_notes.length === btns_notes.length-1) { btns_octave++; }
-}
+
+(function registerHandlers() {
+	for(let i=0; i<spans.length; i++) {
+		const note_name = spans_notes[i % spans_notes.length];
+		const note = `${note_name}${spans_octave}`;
+		spans[i].addEventListener('mouseover', async () => {
+			if(!playing) return;
+			await Tone.start();
+			synth.triggerAttackRelease(note, '4n');
+			// console.log(note);
+		});
+		spans[i].addEventListener('click', async () => { 
+			playing = true; 
+			await Tone.start();
+			synth.triggerAttackRelease(note, '4n');
+		}, { once: true });
+		if(i%spans_notes.length === spans_notes.length-1) { spans_octave++; }
+	}
+
+	for(let i=0; i<btns.length; i++) {
+		const note_name = btns_notes[i % btns_notes.length];
+		const note = `${note_name}${btns_octave}`;
+		btns[i].addEventListener('mouseover', async () => {
+			if(!playing) return;
+			await Tone.start();
+			synth.triggerAttackRelease(note, '4n');
+			// console.log(note);
+		});
+		btns[i].addEventListener('click', async () => { 
+			playing = true; 
+			await Tone.start();
+			synth.triggerAttackRelease(note, '4n');
+		}, { once: true });
+		if(i%btns_notes.length === btns_notes.length-1) { btns_octave++; }
+	}
+})();
