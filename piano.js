ad885a00e62b0a94d5064eec65802e7eea4932aa
Jasper Eenhoorn <een@emmauscollege.nl>
2026-07-01 01:22:09 +0200

    play multiple octaves

--- piano.js
+++ piano.js
@@ -1,20 +1,31 @@
 const synth = new Tone.Synth().toDestination();
 
-const spans_notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"];
-const btns_notes = ["C#4", "D#4", "F#4", "G#4", "A#4"];
+let spans_octave = 4;
+let btns_octave = 4;
+
+const spans_notes = ["C", "D", "E", "F", "G", "A", "B"];
+const btns_notes = ["C#", "D#", "F#", "G#", "A#"];
 
 const spans = document.querySelectorAll("span.white-key");
 for(let i=0; i<spans.length; i++) {
+	const note_name = spans_notes[i % spans_notes.length];
+	const note = `${note_name}${spans_octave}`;
 	spans[i].addEventListener('click', async () => {
 		await Tone.start();
-		synth.triggerAttackRelease(spans_notes[i % spans_notes.length], '4n');
+		synth.triggerAttackRelease(note, '4n');
+		// console.log(note);
 	});
+	if(i%spans_notes.length === spans_notes.length-1) { spans_octave++; }
 }
 
 const btns = document.querySelectorAll("button");
 for(let i=0; i<btns.length; i++) {
+	const note_name = btns_notes[i % btns_notes.length];
+	const note = `${note_name}${btns_octave}`;
 	btns[i].addEventListener('click', async () => {
 		await Tone.start();
-		synth.triggerAttackRelease(btns_notes[i % btns_notes.length], '4n');
+		synth.triggerAttackRelease(note, '4n');
+		// console.log(note);
 	});
+	if(i%btns_notes.length === btns_notes.length-1) { btns_octave++; }
 }
